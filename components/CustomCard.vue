249e8337a89503dee04e0463123e1f44f37482ac
azamat-li <azamatalifullstack@gmail.com>
2020-12-26 02:47:32 +0500

    inited

---
+++ components/CustomCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="card">
+      <span class="span">Card title</span>
+      <button @click="$emit('action-btn:clicked')">Action</button>
+    </div>
+
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
