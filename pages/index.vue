2876e5a9c74141d82a388a2407d0620c1ac3741b
azamat-li <azamatalifullstack@gmail.com>
2020-12-26 20:31:43 +0500

    test(cypress): got e2e work with examle tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,247 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          <strong>Ara</strong>bic <strong>lang</strong>uage
-        </v-card-title>
-        <v-card-text>
-          <p class="intro">Изучай арабский на своём уровне</p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</template>
+  <v-app>
+    <v-app-bar app color="white" height="100">
+      <v-toolbar-title class="font-weight-black headline">
+        AraLang
+      </v-toolbar-title>
+    </v-app-bar>
 
-<script>
-export default {
-  components: {},
-}
-</script>
+    <v-content>
+      <section id="first-look">
+        <v-row no-gutters>
+          <v-theme-provider dark>
+            <v-container fill-height>
+              <v-row
+                align="center"
+                class="white--text mx-auto"
+                justify="center"
+              >
+                <v-col class="white--text text-center" cols="12" tag="h1">
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                    ]"
+                    class="font-weight-light"
+                  >
+                    Учить арабский сегодня.
+                  </span>
+
+                  <br/>
+
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4',
+                    ]"
+                    class="font-weight-black"
+                  >
+                    VUETIFY
+                  </span>
+                </v-col>
+
+                <v-btn
+                  class="align-self-end"
+                  fab
+                  outlined
+                  @click="$vuetify.goTo('#about-me')"
+                >
+                </v-btn>
+              </v-row>
+            </v-container>
+          </v-theme-provider>
+        </v-row>
+      </section>
+
+      <section id="about-me">
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">Знакомство</h2>
+
+          <v-responsive class="mx-auto mb-8" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
+          >
+            Набор оригинальных и переведённых с английского языка постов для
+            изучения арабского языка
+          </v-responsive>
+
+          <div></div>
+
+          <v-btn color="grey" href="#look-throw" large outlined>
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Обзор
+            </span>
+          </v-btn>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="grammarEssentials" class="grey lighten-3">
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">Грамматика</h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ title, text }, i) in grammarEssentials"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                <v-card-title
+                  class="justify-center font-weight-black text-uppercase"
+                  v-text="title"
+                ></v-card-title>
+
+                <v-card-text class="subtitle-1" v-text="text"
+                >Средний уровень
+                </v-card-text
+                >
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="stats">
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+          src="https://unsplash.com/photos/eICUFSeirc0"
+        >
+          <v-container fill-height>
+            <v-row class="mx-auto">
+              <v-col
+                v-for="[value, title] of stats"
+                :key="title"
+                cols="12"
+                md="3"
+              >
+                <div class="text-center">
+                  <div
+                    class="display-3 font-weight-black mb-4"
+                    v-text="value"
+                  ></div>
+
+                  <div
+                    class="title font-weight-regular text-uppercase"
+                    v-text="title"
+                  ></div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-parallax>
+      </section>
+
+      <section id="blog">
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2
+            class="display-2 font-weight-bold mb-3 text-uppercase text-center"
+          >
+            Блог
+          </h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ text, title }, i) in articles"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <h3
+                class="font-weight-black mb-4 text-uppercase"
+                v-text="title"
+              ></h3>
+
+              <div class="title font-weight-light mb-5" v-text="text"></div>
+
+              <v-btn class="ml-n4 font-weight-black" text>
+                Возможно вы найдёте полезным
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <v-sheet id="contact" color="#333333" dark tag="section" tile>
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2
+            class="display-2 font-weight-bold mb-3 text-uppercase text-center"
+          >
+            Сообщить разработчикам
+          </h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-theme-provider light>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field flat label="Имя*" solo></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field flat label="Email*" solo></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field flat label="По поводу*" solo></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-textarea flat label="Сообщить о том, что*" solo></v-textarea>
+              </v-col>
+
+              <v-col class="mx-auto" cols="auto">
+                <v-btn color="accent" x-large> Сообщить</v-btn>
+              </v-col>
+            </v-row>
+          </v-theme-provider>
+        </v-container>
+
+        <div class="py-12"></div>
+      </v-sheet>
+    </v-content>
+
+    <v-footer class="justify-center" color="#292929" height="100">
+      <div
+        class="title font-weight-light grey--text text--lighten-1 text-center"
+      >
+        &copy; {{ new Date().getFullYear() }} — Vuetify, LLC — Made with Love by
+        John Leider
+      </div>
+    </v-footer>
+  </v-app>
+</template>
--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -19,8 +19,8 @@ describe('index.vue', () => {
       localVue,
       vuetify,
       stubs: {
-        NuxtLink: RouterLinkStub
-      }
+        NuxtLink: RouterLinkStub,
+      },
     })
 
     // With jest we can create snapshot files of the HTML output
@@ -28,9 +28,27 @@ describe('index.vue', () => {
 
     // We could also verify this differently
     // by checking the text content
-    const title = wrapper.find(' .intro')
+    const title = wrapper.find('.headline')
 
-    expect(title.text()).toBe('Изучай арабский на своём уровне')
+    expect(title.text()).toBe('AraLang')
   })
 
+  it('should have a custom title and match snapshot', () => {
+    const wrapper = shallowMount(index, {
+      localVue,
+      vuetify,
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    // With jest we can create snapshot files of the HTML output
+    expect(wrapper.html()).toMatchSnapshot()
+
+    // We could also verify this differently
+    // by checking the text content
+    const title = wrapper.find('.headline')
+
+    expect(title.text()).toBe('AraLang')
+  })
 })
