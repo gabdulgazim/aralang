0fd31b325dd622b57fe200f3ea5218bd3c209169
azamat-li <azamatalifullstack@gmail.com>
2020-12-31 01:57:55 +0500

    build: updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,6 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: ['@nuxtjs/axios',
     '@nuxtjs/proxy',
-    ...routerBase
   ],
 
   // to communicate with Laravel backend
